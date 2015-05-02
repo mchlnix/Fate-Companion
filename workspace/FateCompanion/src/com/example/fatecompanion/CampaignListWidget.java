f43433f97f9eb783c239e0e9ef3b70d7469e9a0c
Michael Nix <mchl.nix@googlemail.com>
2015-05-02 17:53:34 +0200

    update CampaignListWidget with all the changes of CharacterListWidget

--- workspace/FateCompanion/src/com/example/fatecompanion/CampaignListWidget.java
+++ workspace/FateCompanion/src/com/example/fatecompanion/CampaignListWidget.java
@@ -1,6 +1,10 @@
 package com.example.fatecompanion;
 
+import java.io.UnsupportedEncodingException;
+import java.util.Random;
+
 import android.content.Context;
+import android.graphics.Color;
 import android.graphics.Typeface;
 import android.util.AttributeSet;
 import android.view.ViewGroup;
@@ -31,17 +35,20 @@ public class CampaignListWidget extends LinearLayout {
 				ViewGroup.LayoutParams.WRAP_CONTENT, 
 				ViewGroup.LayoutParams.MATCH_PARENT ) );
 		
+		// set the width manually, otherwise it would be 1px or as wide as the screen
+		
 		colorSpace.setMinimumWidth( 60 ); // 60 - padding = 50
 		
 		colorSpace.setPadding(0, 0, 10, 0);
 		
-		colorSpace.setImageResource( R.drawable.widget_shape );
+		// what should be shown? color is changed further down (setColorFilter)
 		
-		this.addView( colorSpace );
+		colorSpace.setImageResource( R.drawable.widget_shape );
 		
 		LinearLayout charInfo = new LinearLayout( context );
 		
 		charInfo.setOrientation( LinearLayout.VERTICAL );
+		charInfo.setPadding( 0, 0, 0, 10 );
 		
 		TextView charName = new TextView( context );
 		charName.setTextAppearance(context, android.R.style.TextAppearance_Large);
@@ -61,6 +68,31 @@ public class CampaignListWidget extends LinearLayout {
 			campaign.updateValues( "DebugName", "DebugDescription", RPGSystem.FateCore, 0L, 0L );
 		}
 		
+		/* generate color depending on the character name */
+		
+		// default color if there is a problem with the name
+		
+		int color = Color.rgb( 120, 120, 120 );
+		
+		// seed a RNG with the character name
+		
+		try {
+			byte[] str = campaign.getName().getBytes( "US-ASCII" );
+			
+			String tempName = new String( str );
+			
+			Random gen = new Random( Long.parseLong( tempName, 36 ) );
+			
+			color = Color.rgb( gen.nextInt( 256 ), gen.nextInt( 256 ), gen.nextInt( 256 ));
+			
+		} 
+		catch (NumberFormatException e) {}
+		catch (UnsupportedEncodingException e) {}
+		
+		// set the color of the widget
+		
+		colorSpace.setColorFilter( color );
+		
 		charName.setText( campaign.getName() );
 		charDesc.setText( campaign.getDescription() );
 		charLast.setText( campaign.getLastPlayed().toString() ); //TODO: format correctly
@@ -69,6 +101,7 @@ public class CampaignListWidget extends LinearLayout {
 		charInfo.addView( charDesc );
 		charInfo.addView( charLast );
 		
+		this.addView( colorSpace );
 		this.addView( charInfo );
 	}
 	
