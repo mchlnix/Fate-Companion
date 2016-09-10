a9a15a8813186bd58d16991eb8b5a456ae2de5fd
Michael Nix <mchl.nix@googlemail.com>
2016-09-10 21:41:50 +0200

    adds CS FA Controller class doing nothing

---
+++ workspace/FateCompanion/src/com/example/fatecompanion/controller/CharacterSheetFateAcceleratedController.java
@@ -0,0 +1,12 @@
+package com.example.fatecompanion.controller;
+
+import android.content.Context;
+
+public class CharacterSheetFateAcceleratedController extends CharacterSheetController {
+
+	public CharacterSheetFateAcceleratedController(Context applicationContext) {
+		super(applicationContext);
+		// TODO Auto-generated constructor stub
+	}
+
+}
